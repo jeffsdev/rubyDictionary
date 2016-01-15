10875b550348491e1d933691874dffac7d941a77
Jeff Seymour <seym1585@gmail.com>
2016-01-15 11:00:36 -0800

    add view files for word form and code to append form input to index page

--- app.rb
+++ app.rb
@@ -1,11 +1,20 @@
 require('sinatra')
 require('sinatra/reloader')
-require('./app')
+require('./lib/word')
+also_reload('./lib/**/*.rb')
 
 get('/') do
+  @words = Word.all()
   erb(:index)
 end
 
 get('/add_word') do
   erb(:word_form)
+end
+
+post('/') do
+  term = params.fetch('term')
+  Word.new({:term => term}).save()
+  @words = Word.all()
+  erb(:success)
 end
